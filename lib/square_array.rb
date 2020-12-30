e26d4892c3e9072b422166d18f23a3a49e889af1
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-30 21:16:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,1 @@
-def square_array(array)
-  counter = 0
-  new_array = []
-  while counter < array.length
-    do new_array.push
-    (array[counter]*array[counter] counter +=
-  end
-return new_array
-end
+
